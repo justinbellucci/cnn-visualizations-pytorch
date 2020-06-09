341556c50281668049bfda9a156db99b13d9518c
justinbellucci <justin.bellucci@me.com>
2020-06-09 10:28:06 -0600

    add docstring

--- activation_max_vis_class.py
+++ activation_max_vis_class.py
@@ -2,16 +2,23 @@
 # DATE CREATED: 06_09_2020                                  
 # REVISED DATE: 
 
+import torch
+from torch import optim
+# from torchvision import models, transforms
 import matplotlib.pyplot as plt
 import os
 import numpy as np
-import torch
-from torch import optim
 from helper_functions import process_image, rebuild_image, save_image
 
 class ActivationMaximizationVis():
-    """ 
-    
+    """ Activation Maximization Class based on Erhan et al. (2009) 
+        paper for visualizing filters in a CNN. 
+
+        Attributes:
+            - model (CNN model imported from torchvision)
+            - epochs (int) number of iterations
+            - cnn_layer (int) layer to hook gradients
+            - cnn_filter (int) filter number  
     """
     
     def __init__(self, model, epochs, cnn_layer, cnn_filter):
@@ -34,13 +41,19 @@ class ActivationMaximizationVis():
         """
         def hook_fn(module, grad_in, grad_out):
             self.conv_output = grad_out[0, self.cnn_filter]
+            # saving the number of filters in that layer
+            self.num_filters = grad_out.shape[1]
         self.model[self.cnn_layer].register_forward_hook(hook_fn)
         
     def vis_cnn_layer(self):
+        """ Method to visualize selected filter (activation map) from 
+            a CNN layer. Creates a random image as input. 
+        """
         # initiate hook function
         self.hook_cnn_layer()
         # create a noisy image
-        noisy_img = np.random.randint(125, 190, (224, 224, 3), dtype='uint8')
+        # noisy_img = np.random.randint(125, 190, (224, 224, 3), dtype='uint8')
+        noisy_img = np.uint8(np.random.uniform(150, 180, (224, 224, 3)))
         # add dimension and activate requires_grad on tensor
         processed_image = process_image(noisy_img).unsqueeze_(0).requires_grad_()
         # define optimizer
@@ -56,22 +69,19 @@ class ActivationMaximizationVis():
                 # activate hook when image gets to the layer in question
                 if idx == self.cnn_layer:
                     break
-            # loss function according to Qin et. al
+            # print(self.num_filters)
+            # self.conv_output = x[0, self.cnn_filter]
+            # loss function according to Erhan et al. (2009)
             loss = -torch.mean(self.conv_output)
             loss.backward() # calculate gradients
             optimizer.step() # update weights
             self.layer_img = rebuild_image(processed_image) # reconstruct image
-            # print(type(self.layer_img))
-            # print(self.layer_img.shape)
-            print('Epoch {}/{} --> Loss {:.3f}'.format(e+1, self.epochs, loss.data.numpy()))
             
+            print('Epoch {}/{} --> Loss {:.3f}'.format(e+1, self.epochs, loss.data.numpy()))
+
             if e % 10 == 0:
-                img_path = 'activ_max_imgs/am_layer_' + str(self.cnn_layer) + \
-                    '_f_' + str(self.cnn_filter) + '_iter' + str(e+1) + '.jpg'
-                plt.imshow(self.layer_img)
-                plt.axis('off')
-                plt.savefig(img_path, dpi=150)
-                # save_image(self.layer_img, img_path)
-                # imsave(img_path, self.layer_img)
-                plt.show()
+                img_path = 'activ_max_imgs/am_vis_l' + str(self.cnn_layer) + \
+                    '_f' + str(self.cnn_filter) + '_iter' + str(e+1) + '.jpg'
+                save_image(self.layer_img, img_path)
+
     
